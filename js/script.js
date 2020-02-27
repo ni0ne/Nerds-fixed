ae5cb35b6ab94c35692e3e51797107b432e5689e
ni0ne <40385165+ni0ne@users.noreply.github.com>
2020-02-27 15:53:45 +1000

    Release

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var link = document.querySelector(".write-us");
+
+var popup = document.querySelector(".modal-contacts");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slider-item");
+    var dots = document.getElementsByClassName("slider-dots");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" current", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " current";
+}
